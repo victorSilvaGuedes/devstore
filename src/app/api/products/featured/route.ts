0a8a530382621483c28d58f867d2effd19374d67
Victor Silva Guedes <sguedes.victor@gmail.com>
2024-05-22 20:29:29 -0300

    feat: comments

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -2,6 +2,8 @@ import data from '../data.json'
 
 export async function GET() {
   await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  // encontra os produtos no .json baseado se featured = true
   const featuredProduct = data.products.filter((product) => product.featured)
   return Response.json(featuredProduct)
 }
