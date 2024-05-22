4916e28520dcf0f1b85d6fbad84cf2bf2a11d356
Victor Silva Guedes <sguedes.victor@gmail.com>
2024-05-22 20:29:16 -0300

    feat: cart provider

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/components/header'
+import { CartProvider } from '@/context/cart-context'
 
 export default function LayoutStore({
   children,
@@ -6,9 +7,11 @@ export default function LayoutStore({
   children: React.ReactNode
 }) {
   return (
-    <div className="text-zinc-100 mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="text-zinc-100 mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   )
 }
